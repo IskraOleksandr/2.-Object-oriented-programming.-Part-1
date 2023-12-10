0b62c1ee67274d2ab67cef0a26a10fc3e2e76fd5
_Iskra_ <_Iskra_@DESKTOP-4SUHQ4F>
2023-12-10 15:36:37 +0200

    #Task6 Изменение даных для заполнения классов.

--- src/Ex5.java
+++ src/Ex5.java
@@ -2,24 +2,27 @@ import Classes.Book;
 
 public class Ex5 {
     public static void main(String[] args) {
-        
+
         //Конструктор без параметров
         Book book = new Book();
         //set
-        book.setTitle(" Книга");
-        book.setAuthor("Виталий Сергеевич");
-        book.setYear(2010);
-        book.setPublisher("Киев");
-        book.setGenre("Украина");
-        book.setPageCount(70);
+        book.setTitle("Гарри Поттер и философский камень");
+        book.setAuthor("Джоан Роулинг");
+        book.setYear(1997);
+        book.setPublisher("Bloomsbury Publishing");
+        book.setGenre("Фэнтези");
+        book.setPageCount(332);
 
-        System.out.println("\nДанные получаные с помощью методов get: \nНазвание книги:" + book.getTitle() + "\nФИО автора: " + book.getAuthor()
-                + "\nГод выпуска: " + book.getYear() + "\nНазвание издательства: " + book.getPublisher() +
-                "\nЖанр книги: " + book.getGenre() + "\nКоличество страниц: " + book.getPageCount());
+        System.out.println("\nДанные получаные с помощью методов get: \nНазвание книги:" + book.getTitle() +
+                "\nФИО автора: " + book.getAuthor() + "\nГод выпуска: " + book.getYear() +
+                "\nНазвание издательства: " + book.getPublisher() + "\nЖанр книги: " +
+                book.getGenre() + "\nКоличество страниц: " + book.getPageCount());
 
 
         //Конструктор с параметрами
-        Book book1 = new Book("", "Сергей ", 2016, "Львов", "Украина", 100);
+        Book book1 = new Book("Гарри Поттер и Тайная комната", "Джоан Роулинг ", 1998,
+                "Bloomsbury Publishing", "Фэнтези", 345);
+        
         System.out.println("\nДанные получанные перегруженым методом toString() " + book1);
 
 
--- src/Ex6.java
+++ src/Ex6.java
@@ -2,20 +2,20 @@ import Classes.Car;
 
 public class Ex6 {
     public static void main(String[] args) {
-        
+
         //Конструктор без параметров
         Car car = new Car();
         //set
-        car.setCarName("Nisan");
-        car.setManufacturer("Центр");
-        car.setYear(2010);
-        car.setEngineVolume(100);
+        car.setCarName("Focus");
+        car.setManufacturer("Ford");
+        car.setYear(2019);
+        car.setEngineVolume(1.8);
 
         System.out.println("\nДанные получаные с помощью методов get: \nНазвание: " + car.getCarName() + "\nПроизводитель: " + car.getManufacturer() +
                 "\nГод выпуска: " + car.getYear() + "\nОбъем двигателя: " + car.getEngineVolume());
 
         //Конструктор с параметрами
-        Car car1 = new Car("Subaru", "Центр", 2013, 300);
+        Car car1 = new Car("Camry", "Toyota", 2018, 2.5);
         System.out.println("\nДанные получанные перегруженым методом toString() " + car1);
 
         Car car2 = new Car();
